79a2888492529ba35df7134b283e69682177e960
eykamp <chris@eykamp.com>
2015-09-25 14:42:57 -0700

    Add a graph for snorkelfish

--- DataTracker/js/chartservices.js
+++ DataTracker/js/chartservices.js
@@ -9,24 +9,28 @@ var cmod = angular.module('ChartServices', []);
 
 /*
 		Each dataset type that you want to provide a generated graph needs to live here.
-		You can do your graphing however you like for the particular kind of chart.
+		You can do your graphing however you like for the particular kind of chart.d
 */
-cmod.service('ChartService', ['AdultWeir_ChartService','WaterTemp_ChartService',
-	function(AdultWeir_ChartService, WaterTemp_ChartService){
+cmod.service('ChartService', ['AdultWeir_ChartService','WaterTemp_ChartService','SnorkelFish_ChartService',
+	function(AdultWeir_ChartService, WaterTemp_ChartService, SnorkelFish_ChartService){
 		var service = {
 			buildChart: function(scope, data_in, dataset, config){
 
     				if(dataset == "AdultWeir") 
 					{
 						scope.chartConfig = AdultWeir_ChartService.getChartConfig();
-    					//$scope.chartData = AdultWeir_ChartService.getDefaultChartData();
 		    			scope.chartData = AdultWeir_ChartService.getChartData(data_in);
 					}
 		    		else if(dataset == "WaterTemp")
 		    		{
 		    			WaterTemp_ChartService.buildChart(data_in, config);
 		    		}
-		    		//else
+		    		else if(dataset == "SnorkelFish")
+		    		{
+		    			// SnorkelFish_ChartService.buildChart(data_in, config);
+		    			scope.chartConfig = SnorkelFish_ChartService.getChartConfig();
+		    			scope.chartData   = SnorkelFish_ChartService.getChartData(data_in);
+		    		}		    		//else
 		    		//	delete $scope.chartData; 
 		    },
 		};
@@ -241,5 +245,255 @@ cmod.service('WaterTemp_ChartService',[
 ]);
 
 
+cmod.service('SnorkelFish_ChartService',[ 
+    function(){
+		console.log("Inside SnorkelFish_ChartService...");
+        var service = {
+
+        	dataset: "SnorkelFish",
+
+			getChartConfig: function(){
+				var config = {
+    			  title : 'Fish Count',
+				  tooltips: true,
+				  labels : false,
+				  
+				  legend: {
+				    display: true,
+				    position: 'right'
+				  }
+				};
+
+				return config;
+			},
+
+
+			getDefaultChartData: function()
+			{
+				var defaultChartData = {"series": [], "data":[{ "x": "Loading...", "y": [0],"tooltip": ""}]}; //default
+				return defaultChartData;
+			},
+
+
+			getChartData: function(data)
+			{
+			    var dataCalc = {};
+
+			    angular.forEach(data, function(row, key){
+			        //var num = (row.TotalFishRepresented) ? row.TotalFishRepresented : 1; // From AdultWeir
+			        var num = row.FishCount;
+			        console.log("row is next...");
+					console.dir(row);
+					console.log("num = " + num);
+
+			        if(row.Species)
+			        {
+						console.log("Inside row.Species if...");
+			            if(!dataCalc[row.Species])
+			                dataCalc[row.Species] = { total: 0 };
+
+			            dataCalc[row.Species].total += num;
+			        }
+			    });
+
+			    var data = {
+			              "series": ["Total"],
+			              "data": [] 
+			          };
+
+			    angular.forEach(dataCalc, function(vals, species){
+			        data['data'].push({
+			          "x": species,
+			          "y": [vals.total],
+			        });
+			    });
+
+				console.log("data is next...");
+			    console.dir(data);
+
+			    return data;
+
+			},
+
+			buildChart: function(){
+
+			},
+
+        };
+
+        return service;
+    }
+]);
+
+
+
+// cmod.service('SnorkelFish_ChartService',[ 
+//     function(){
+//         var service = {
+
+//         	dataset: "SnorkelFish",
+
+// 			getChartConfig: function(){
+// 				var config = {
+//     			  title : 'Fish Count',
+// 				  tooltips: true,
+// 				  labels : false,
+				  
+// 				  legend: {
+// 				    display: true,
+// 				    position: 'right'
+// 				  }
+// 				};
+
+// 				return config;
+// 			},
+
+
+// 			getDefaultChartData: function()
+// 			{
+// 				var defaultChartData = {"series": [], "data":[{ "x": "Loading...", "y": [0],"tooltip": ""}]}; //default
+// 				return defaultChartData;
+// 			},
+
+
+// 			getChartData: function(data)
+// 			{
+// 			    var dataCalc = {};
+
+// 			    angular.forEach(data, function(row, key){
+// 			        var fishCount = (row.FishCount) ? row.FishCount : 1;
+// 			        //console.log(row);
+
+// 			        if(row.Species)
+// 			        {
+
+// 			            if(!dataCalc[row.FishCount])
+// 			                dataCalc[row.FishCount] = 0;
+
+// 			            dataCalc[row.FishCount].total += fishCount;
+
+// 			        }
+// 			        //console.dir(dataCalc[row.Species]);
+			        
+// 			    });
+
+// 			    var data = {
+// 			              "series": [
+// 			                "Fish Count"
+// 			              ],
+// 			              "data": [
+// 			              ]
+// 			            };
+
+// 			    angular.forEach(dataCalc, function(vals, species){
+// 			        data['data'].push({
+// 			          "x": species,
+// 			          "y": [vals.total],
+// 			        });
+// 			    });
+
+// 			    return data;
+
+// 			},
+
+// 			buildChart: function(data_in, config){
+// 				if(data_in.length == 0)
+// 					return;
+
+// 				if(!config)
+// 					config = {width: 400, height: 200};
+
+// 					var margin = {top: 10, right: 10, bottom: 20, left: 30},
+// 					    width = config.width - margin.left - margin.right,
+// 					    height = config.height - margin.top - margin.bottom;
+
+// 					var x = d3.time.scale()
+// 					    .range([0, width]);
+
+// 					var y = d3.scale.linear()
+// 					    .range([height, 0]);
+
+// 					var color = d3.scale.ordinal()
+// 					  		.domain([1,12,13,14,15,16])
+// 					  		.range(["FF0000","#009933" , "#0000FF","#0FF933" , "#00FFFF","#00FFAAFB"]);
+
+// 					var xAxis = d3.svg.axis()
+// 					    .scale(x)
+// 					    .orient("bottom");
+
+// 					var yAxis = d3.svg.axis()
+// 					    .scale(y)
+// 					    .orient("left");
+
+// 					var line = d3.svg.line()
+// 						//.interpolate("basis")
+// 					    .x(function(d) { return x(d.chart_date); })
+// 					    .y(function(d) { return y(d.chart_count); });
+
+// 					    d3.select("#chart-div").selectAll("svg").remove();
+
+// 					var svg = d3.select("#chart-div").append("svg")
+// 					    .attr("width", width + margin.left + margin.right)
+// 					    .attr("height", height + margin.top + margin.bottom)
+// 					  .append("g")
+// 					    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	
+// 					//converting via new Date() seems to work better.
+// 					//var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S").parse;
+
+// 					//color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+
+// 					var data = [];
+// 					data.Vals = {};
+// 					data.MaxCount = 0;
+
+// 					data_in.forEach(function(d) {
+// 					  	if(!isNaN(d.FishCount) && d.Species != '')
+// 					  	{
+// 					  		if(!data.Vals[d.Species])
+// 					  			data.Vals[d.Species] = 0;
+
+// 					  		data.Vals[d.Species] += d.FishCount;
+
+// 					  		if(data.Vals[d.Species] > data.MaxCount)
+// 					  			data.MaxCount = data.Vals[d.Species];
+// 					    }
+// 					  });
+
+
+// 					  x.domain(d3.extent(data, function(d) { return Object.keys(d.Vals); }));
+// 					  y.domain(d3.extent(data, function(d) { return d.MaxCount; }));
+
+// 					var chart = d3.select(".chart")
+// 					    .attr("width", width)
+// 					    .attr("height", height);
+
+// 					d3.tsv("data.tsv", type, function(error, data) {
+// 					  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+// 					  var barWidth = width / data.length;
+
+// 					  var bar = chart.selectAll("g")
+// 					      .data(data)
+// 					    .enter().append("g")
+// 					      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+
+// 					  bar.append("rect")
+// 					      .attr("y", function(d) { return y(d.value); })
+// 					      .attr("height", function(d) { return height - y(d.value); })
+// 					      .attr("width", barWidth - 1);
+
+// 					  bar.append("text")
+// 					      .attr("x", barWidth / 2)
+// 					      .attr("y", function(d) { return y(d.value) + 3; })
+// 					      .attr("dy", ".75em")
+// 					      .text(function(d) { return d.value; });
+
+//         };
+
+//         return service;
+//     }
+// ]);
 
 
