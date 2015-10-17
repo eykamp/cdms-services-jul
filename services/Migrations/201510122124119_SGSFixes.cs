87b652aed6aa4dc8ad830dc01220a78b11b83001
eykamp <chris@eykamp.com>
2015-10-16 17:00:34 -0700

    Various data fixes

--- services/Migrations/201510122124119_SGSFixes.cs
+++ services/Migrations/201510122124119_SGSFixes.cs
@@ -12,6 +12,8 @@ namespace services.Migrations
     update fields set PossibleValues = '{""REDD"":""Redd"", ""LIVE"":""Live"", ""CARCASS"":""Carcass"", ""OBSERVATION"":""Observation""}' where name = 'Feature Type'
     update fields set PossibleValues = '{""M"":""Middle"", ""S"":""Side""}' where name = 'Redd Location'
     update fields set PossibleValues = '{""M"":""Male"",""F"":""Female"",""UNK"":""Unknown""}' where name = 'Sex'
+    update fields set PossibleValues = '{""ONR"":""On Redd"", ""NR"":""Near Redd"", ""OR"":""Off Redd""}' where name = 'Fish Location'
+    update fields set PossibleValues = '{""P"":""Pool"", ""TO"":""Pool Tail Out"", ""RI"":""Riffle"", ""GL"":""Glide""}' where name = 'Redd Habitat'
 ");
         }
         
@@ -22,6 +24,8 @@ namespace services.Migrations
     update fields set PossibleValues = '[""Redd"", ""Live"", ""Carcass"", ""Observation""]' where name = 'Feature Type'
     update fields set PossibleValues = '[""Middle (M)"", ""Side (S)""]' where name = 'Redd Location'
     update fields set PossibleValues = '[""M"",""F"",""UNK""]' where name = 'Sex'
+    update fields set PossibleValues = '[""On Redd (OR)"", ""Near Redd (NR)"", ""Off Redd (OR)""]' where name = 'Fish Location'
+    update fields set PossibleValues = '[""Pool (P)"", ""Pool Tail Out (TO)"", ""Riffle (RI)"", ""Glide (GL)""]' where name = 'Redd Habitat'
 ");
         }
     }
--- services/Models/Data/WaterQuality_Header.cs
+++ services/Models/Data/WaterQuality_Header.cs
@@ -5,5 +5,6 @@ namespace services.Models.Data
     public class WaterQuality_Header : DataHeader
     {
         public string DataType { get; set; }
+        public string FieldsheetLink { get; set; }
     }
 }
